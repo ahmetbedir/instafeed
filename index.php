81c297f6ccf11a0a89071142d36bd6ab3c50d1cc
Ahmet Bedir <ahmetbedir16@gmail.com>
2020-04-20 06:30:41 +0300

    beta

--- index.php
+++ index.php
@@ -6,13 +6,8 @@ use Ahmetbedir\InstaFeed;
 
 $instafeed = new InstaFeed('ahmetbdr43');
 
-echo '<pre>';
-var_dump($instafeed->getFirstFeed());
-
-die;
-
 echo '<div style="display:flex; flex-flow: row wrap;justify-content: space-around;">';
-foreach ($feed->getFeedList() as $feed) {
+foreach ($instafeed->getFeedList() as $feed) {
     echo '<img src="' . $feed->getThumbnailByIndex(2) . '" />';
 }
 echo '</div>';
